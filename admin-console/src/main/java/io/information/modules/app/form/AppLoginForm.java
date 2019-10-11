cea99f3cf74b118c4d12172bca1d7be5d01a8136
liuxue086 <liuxue086@163.com>
2019-10-11 17:00:15 +0800

    帖子投票，注册登录bug修复

---
+++ admin-console/src/main/java/io/information/modules/app/form/AppLoginForm.java
@@ -0,0 +1,34 @@
+
+
+package io.information.modules.app.form;
+
+import com.guansuo.validgroups.CodeLogin;
+import com.guansuo.validgroups.PwdLogin;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+
+/**
+ * App登录表单
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Data
+@ApiModel(value = "登录表单")
+public class AppLoginForm {
+    @ApiModelProperty(value = "手机号")
+    @NotBlank(message="请输入手机号")
+    private String uPhone;
+
+    @ApiModelProperty(value = "密码")
+    @NotBlank(message="请输入密码",groups = {PwdLogin.class})
+    private String uPwd;
+
+    @ApiModelProperty(value = "验证码")
+    @NotBlank(message="请输入验证码",groups = {CodeLogin.class})
+    private String code;
+
+}
--- admin-console/src/main/java/io/information/modules/app/service/impl/InCardVoteServiceImpl.java
+++ admin-console/src/main/java/io/information/modules/app/service/impl/InCardVoteServiceImpl.java
@@ -1,9 +1,12 @@
 package io.information.modules.app.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.information.common.utils.RedisKeys;
 import io.information.modules.app.dao.InCardVoteDao;
 import io.information.modules.app.entity.InCardVote;
 import io.information.modules.app.service.IInCardVoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +19,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class InCardVoteServiceImpl extends ServiceImpl<InCardVoteDao, InCardVote> implements IInCardVoteService {
+   @Autowired
+   InCardVoteDao inCardVoteDao;
 
+    @Override
+    @CachePut(value= RedisKeys.VOTE,key = "#cid+'-'+#uid+'-'+#optIndex")
+    public boolean vote(Long cid, Long uid, Integer optIndex) {
+        try {
+            inCardVoteDao.addVote(cid,optIndex);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
