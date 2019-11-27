7a0426495d01ecf0dd9b7525a5864bbde0f44477
liuxue086 <liuxue086@163.com>
2019-11-27 18:40:03 +0800

    netty

---
+++ admin-console/src/main/java/io/information/netty/socket/WebsocketServer.java
@@ -0,0 +1,54 @@
+package io.information.netty.socket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Websocket
+ * 
+ * @author LX
+ * @date 2019-11-25
+ */
+public class WebsocketServer {
+    protected Logger LOG = LoggerFactory.getLogger(getClass());
+
+    private int port;
+
+    public WebsocketServer(int port) {
+        this.port = port;
+    }
+
+
+    public void run() throws Exception {
+        
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class) // (3)
+             .childHandler(new WebsocketServerInitializer())  //(4)
+             .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+             .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            LOG.info("---------------WebsocketChatServer 启动了---------------" + port);
+            // 绑定端口，开始接收进来的连接
+            ChannelFuture f = b.bind(port).sync(); // (7)
+            // 等待服务器  socket 关闭 。
+            // 在这个例子中，这不会发生，但你可以优雅地关闭你的服务器。
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+            LOG.info("---------------WebsocketChatServer--------------- 关闭了");
+        }
+    }
+
+}
---
+++ admin-console/src/main/java/io/information/netty/util/ChannelUtil.java
@@ -0,0 +1,89 @@
+package io.information.netty.util;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class ChannelUtil {
+    /**
+     * 用于记录c-s连接后建立的通道
+     */
+    private static final Set<Channel> CHANNELS = new CopyOnWriteArraySet<>();
+    //    private static final Set<Channel> CHANNELS = new ConcurrentSkipListSet<>();
+    private static final Map<String,Channel> CLIENT_CHANNELS=new ConcurrentHashMap();
+
+    /**
+     * ChannelUtil日志输出
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(ChannelUtil.class);
+    /**
+     * 用于记录通道响应的结果集
+     */
+    private static final Map<String, Object> RESULT_MAP = new ConcurrentHashMap<>();
+
+    private ChannelUtil() {
+    }
+
+    /**
+     * 计算结果集（存储响应结果）
+     *
+     * @param key    唯一标识
+     * @param result 结果集
+     */
+    public static void calculateResult(String key, Object result) {
+        RESULT_MAP.put(key, result);
+    }
+
+    /**
+     * 获取结果集的key
+     *
+     * @param key 保存的唯一标识
+     * @return 结果集的 key (通道标识)
+     */
+    public static String getResultKey(String key) {
+        return (String) getResult(key);
+    }
+
+    /**
+     * 根据结果集的 key 获取结果集
+     *
+     * @param key 结果集的key
+     * @return 结果集
+     */
+    public static Object getResult(String key) {
+        return RESULT_MAP.get(key);
+    }
+
+    /**
+     * 注册通道
+     *
+     * @param channel 通道
+     */
+    public static void registerChannel(Channel channel) {
+        ChannelId id = channel.id();
+        LOG.info("{} -> [添加通道] {}", ChannelUtil.class.getName(), id);
+        boolean add = CHANNELS.add(channel);
+        LOG.info("添加通道结果："+add);
+    }
+
+    public static void bind(String clientName,Channel channel)
+    {
+        CLIENT_CHANNELS.put(clientName,channel);
+    }
+
+    public static Channel getClientChannel(String key)
+    {
+        return ChannelUtil.CLIENT_CHANNELS.get(key);
+    }
+
+    public static int getClientChannelSize()
+    {
+        return CLIENT_CHANNELS.size();
+    }
+}
